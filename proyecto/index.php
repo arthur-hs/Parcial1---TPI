c164e81a047749cb5d62e09c84ea0a3a9a39aa04
arthur-hs <luisgaleas2205@gmail.com>
2026-09-02 12:46:43 -0600

    FEAT: Creacion de la estructura html

---
+++ proyecto/index.php
@@ -0,0 +1,86 @@
+<php?
+
+start_session();
+
+
+
+?>
+
+
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registro de Inscripciones</title>
+    </head>
+
+    <body>
+
+        <h1>Registro de inscripciones de Participantes </h1>
+
+        <form action="" method="post" name="participantes">
+
+            <div>
+                <label for="">Ingrese su Nombre</label>
+                <input type="text" name="nombreEstudiante">
+            </div>
+
+            <div>
+                <label for="">Ingrese su Edad</label>
+                <input type="number" name="edadEstudiante">
+            </div>
+
+            <div>
+                <label for="">Ingrese su Correo</label>
+                <input type="gmail" name="nombreEstudiante">
+            </div>
+
+            <div>
+                <label for="">Ingrese su VideoJuego</label>
+                <input type="text" name="nombreEstudiante">
+            </div>
+
+            <div>
+                <label for="">Ingrese su Modalidad</label>
+                <input type="text" name="nombreEstudiante">
+            </div>
+
+            <div>
+                <label for="">Ingrese su Nivel de Experiencia</label>
+                <input type="text" name="nombreEstudiante">
+            </div>
+
+            <div>
+                <button>Registrar Participante</button>
+            </div>
+        </form>
+
+        <h2>Participantes Registrados en el sistema..</h2>
+
+        <table>
+            <thead>
+                <th>
+                    <tr>#</tr>
+                    <tr>Nombre</tr>
+                    <tr>Edad</tr>
+                    <tr>Correo</tr>
+                    <tr>Video Juego </tr>
+                    <tr>Mpdalidad</tr>
+                    <tr>Nivel</tr>
+                    <tr>Experiencia</tr>
+                </th>
+            </thead>
+
+            <tbody>
+                <td>
+                    <tr></tr>
+                </td>
+            </tbody>
+        </table>
+
+    </body>
+
+    </html>
